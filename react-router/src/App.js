19a683e4a6889646f80c1407ef3a2f4925283131
unknown <frankortiz5249@gmail.com>
2024-04-18 22:18:58 -0700

    completed montys mineral spa intro to react router dom activity

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,42 +1,53 @@
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
 
 import './App.css';
 
-import Home from './components/Home'
+import Home from './components/Home.js'
 import About from './components/About'
 import Packages from './components/Packages'
 
 
 function App() {
 
-  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
+  const packages = ['Activate your Crystals', 
+  'Monkey Meditation', 
+  'Soak in the Hotsprings', 
+  'Hypnotherapy', 
+  'Mineral Bath']
 
-  return (
-    <div className="App">
+return (
+  <div classname="App">
+    <Router>
       <header>
-        <h1 className="title">Welcome to Monty's Mineral SPA</h1>
-
-        <div className="navBar">
+        <div classname="navBar">
           <ul>
             <li>
-              <a href={<Home />}>Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <a href={<About />}>About Us</a>
+              <Link to="/about">About</Link>
             </li>
             <li>
-              <a href={<Packages packages={packages}/>}>Our Packages</a>
+              <Link to="/packages">Our Packages</Link>
             </li>
           </ul>
         </div>
-
       </header>
-    </div>
-  );
+      <div className="display">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/packages" element={<Packages packages={packages}/>} />
+        </Routes>
+      </div>
+    </Router>
+  </div>
+);
 }
 
 export default App;
 
-
+// all the paths work and the text is showing the only thing not showing is the BG image from the repo
 
 
 
